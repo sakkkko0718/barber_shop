7dca041830d2afa3385e63cd2b3a87740e427213
sakkkko0718 <sakikonakanishi17@gmail.com>
2024-02-20 13:27:58 +0900

    レスポンシブ変更

--- resources/views/reservations/reservations.blade.php
+++ resources/views/reservations/reservations.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.adminbase')
 
 @section('content')
-        <table  class="table-striped table">
-            <tr class="menu">
+        <table  class="tbl-r04">
+            <tr class="thead">
                 <th></th>
                 <th>予約ID</th>
                 <th>ゲストID</th>
@@ -47,4 +47,35 @@
         font-size: 18px;
         height: 5vh;
     }
+
+    @media screen and (max-width: 640px) {
+  .tbl-r04 {
+    width: 80%;
+  } 
+  .tbl-r04 .thead {
+    display: none;
+  }
+  .tbl-r04 tr,
+  .tbl-r04 td{
+    display: block;
+    width: 100%;
+    position: relative;
+  }
+  .tbl-r04 td:first-child:before {
+    background: #e9727e;
+    box-sizing: border-box;
+    color: #fff;
+    font-weight: bold;
+    height: 40px;
+    padding: 10px;
+    position: absolute;
+    top: -41px;
+    left: -0.2%;
+    display: block;
+    width: 100.5%;
+  }
+  .tbl-r04 tr {
+    margin-bottom: 40px;
+  }
+}
 </style>
